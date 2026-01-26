cd05d2e10414e9d55fedac8623e9358b41eca4ec
HVA216204TYK37204 <nguyenthidinh652@gmail.com>
2026-01-26 13:39:11 +0700

    Update models.py

--- models.py
+++ models.py
@@ -2,7 +2,6 @@ import torch.nn as nn
 from torchvision import models
 import timm
 
-
 def build_densenet121(num_classes: int = 4):
     model = models.densenet121(pretrained=False)
     num_ftrs = model.classifier.in_features
@@ -13,7 +12,6 @@ def build_densenet121(num_classes: int = 4):
         nn.Linear(512, num_classes)
     )
     return model
-
 
 def build_vit(model_name: str, num_classes: int = 4):
     model = timm.create_model(model_name, pretrained=False, num_classes=num_classes)
